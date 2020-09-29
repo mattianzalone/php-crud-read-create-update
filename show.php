1b3c20c891d627cfeb25063178e4eeb60ae18fda
mattia <mattia24.studio@gmail.com>
2020-09-29 19:54:02 +0200

    added

---
+++ show.php
@@ -0,0 +1,17 @@
+<?php
+include __DIR__.'/partials/template/header.php';
+include __DIR__.'/partials/show/server.php';
+?>
+<body>
+    <div class="container">
+        <ul class="list-group">
+            <li class="list-group.item">ID: <?php echo $row['id']; ?>  </li>
+            <li class="list-group.item">Numero della stanza: <?php echo $row['room_number'] ?> </li>
+            <li class="list-group.item">Piano: <?php echo $row['floor'] ?> </li>
+            <li class="list-group.item">Numeri di letti: <?php echo $row['beds'] ?></li>
+            <a href="index.php">INDIETRO</a>
+        </ul>
+    </div>
+
+</body>
+</html>
